501b9223c82f54e7c30e9ac709c5b8293acf3913
matheusbarb <matheusbarbosacontato00@gmail.com>
2024-05-14 12:58:18 -0300

    URL AUTH ATT

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,26 @@
-
+import Image from "next/image";
 import Link from "next/link";
-
+import { useSession } from "next-auth/react";
 
 export default function Home() {
-  
+  const { data: session } = useSession();
 
   return (
     
     <div className="flex flex-col justify-center h-[calc(100vh-130px)] ">
-      
+      {session ? (
         <div className="">
           <Link href={"/dashboard"}>
             <div className="flex flex-col justify-center items-center ">
               <section className="bg-black rounded-md ">
-                <span className="bg-[#C4A1FF] block p-2 -translate-x-1 -translate-y-1 border-black border-2 rounded-md text-xl hover:-translate-x-2 hover:-translate-y-2 transition-all">
+                <span className="bg-[#C4A1FF] block p-2 -translate-x-1 -translate-y-1 border-black border-2 rounded-md text-xl hover:-translate-x-2 hover:-translate-y-2 transition-all active:translate-x-0 active:translate-y-0">
                   Meu Painel
                 </span>
               </section>
             </div>
           </Link>
         </div>
-    
+      ) : null}
       <div className="flex flex-col items-center">
         <h1>COOL STACKS (BETA)</h1>
         <h1 className="flex-grow font-bold text-2xl text-center ">
